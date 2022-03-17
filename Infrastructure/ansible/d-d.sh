f9b5cfced07a218d57f83d4022001b449a30097a
Liakhovych Dmytro <dimoniq@gmail.com>
2022-03-17 22:21:26 +0100

    Rename yamls

--- Infrastructure/ansible/d-d.sh
+++ Infrastructure/ansible/d-d.sh
@@ -1,4 +1,4 @@
 ansible-playbook --inventory-file inventory \
 --extra-vars "directus_cms_key=$DIRECTUS_CMS_KEY directus_cms_secret=$DIRECTUS_CMS_SECRET directus_cms_admin_email=$DIRECTUS_CMS_ADMIN_EMAIL directus_cms_admin_password=$DIRECTUS_CMS_ADMIN_PASSWORD" \
-directus-deployment.yml \
+directus-deployment.yaml \
 -kK
